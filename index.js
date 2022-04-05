b01e47c30f6ba3a2d1d097cd4fa234633663913c
choewy <choewy32@gmail.com>
2022-04-05 16:36:16 +0900

    index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const sqlite3 = require('sqlite3');
+
+class Sqlite3Promise {
+    connect(filename) {
+        this.db = new sqlite3.Database(filename);
+    };
+
+    getAllQuery = (query, params = []) =>
+        new Promise((resolve, reject) => {
+            this.db.all(query, params, (err, rows) => {
+                if (err) reject(err);
+                resolve(rows);
+            });
+        });
+
+    getOneQuery = (query, params = []) =>
+        new Promise((resolve, reject) => {
+            this.db.get(query, params, (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+
+    runQuery = (query, params = []) =>
+        new Promise((resolve, reject) => {
+            this.db.run(query, params, (err) => {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+};
+
+const Database = new Sqlite3Promise();
+
+module.exports = Database;
